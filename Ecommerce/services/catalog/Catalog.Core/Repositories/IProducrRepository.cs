e52ce0a5da30769feea005675d8afae5d3badbf4
Aly-Abozaid <aly@appnest.pro>
2026-02-26 07:31:04 +0200

    Create Repositories (interfaces) in Catalog microservice

---
+++ Ecommerce/services/catalog/Catalog.Core/Repositories/IProducrRepository.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Core.Repositories
+{
+    public interface IProducrRepository
+    {
+        Task<IEnumerable<Product>> GetAllProducts();
+        Task<IEnumerable<Product>> GetAllProductsByName( string name );
+        Task<IEnumerable<Product>> GetAllProductsByBrand( string name );
+        Task<Product> GetProductById( string id);
+        Task<bool> DeleteProduct( string id);
+        Task<bool> UpdateProduct( Product product);
+        Task<Product> CreateProduct( Product product);
+    }
+}
