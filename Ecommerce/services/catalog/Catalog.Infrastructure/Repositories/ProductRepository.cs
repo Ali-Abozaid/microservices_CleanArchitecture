4edbf3328ed7ef0e7f375c7d0a827a4633d94e6b
Ali-Abozaid <ali.abozaidazz@gmail.com>
2026-03-21 20:01:05 +0200

    make folder Repositories in infrastructure to implement the repos in core

---
+++ Ecommerce/services/catalog/Catalog.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,63 @@
+﻿using Catalog.Core.Entities;
+using Catalog.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Infrastructure.Repositories
+{
+    public class ProductRepository : IProductRepository, IBrandRepository, ITypeRepository
+    {
+        // IProductRepository
+        public Task<Product> CreateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteProduct(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetAllProductsByBrand(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetAllProductsByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+        public Task<Product> GetProductById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        // ITypeRepository
+        public Task<IEnumerable<ProductType>> GetAllTypes()
+        {
+            throw new NotImplementedException();
+        }
+        // IBrandRepository
+        public Task<IEnumerable<ProductBrand>> GetAllBrands()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
