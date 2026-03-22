e983f48e20ff1049434584a3f5f6418dacbac8a8
Ali-Abozaid <ali.abozaidazz@gmail.com>
2026-03-22 21:19:42 +0200

    implement CQRS GetAllProductsByNameQueryHandler

---
+++ Ecommerce/services/catalog/Catalog.Application/Handlers/Queries/GetAllProductsByNameQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using Catalog.Core.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.Handlers.Queries
+{
+    public class GetAllProductsByNameQueryHandler : IRequestHandler<GetAllProductsByNameQuery, IList<ProductResponseDto>>
+    {
+        private readonly IMapper _mapper;
+        private readonly IProductRepository _productRepository;
+        public GetAllProductsByNameQueryHandler(IMapper mapper, IProductRepository productRepository)
+        {
+            _mapper = mapper;
+            _productRepository = productRepository;
+        }
+        public async Task<IList<ProductResponseDto>> Handle(GetAllProductsByNameQuery request, CancellationToken cancellationToken)
+        {
+            var products = _productRepository.GetAllProductsByName(request.Name);
+            var productsResponse = _mapper.Map<IList<ProductResponseDto>>(products);
+            return productsResponse;
+        }
+    }
+}
---
+++ Ecommerce/services/catalog/Catalog.Application/Queries/GetAllProductsByNameQuery.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.Application.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.Queries
+{
+    public class GetAllProductsByNameQuery:IRequest<IList<ProductResponseDto>>
+    {
+        public string Name { get; set; }
+        public GetAllProductsByNameQuery(string name)
+        {
+            Name = name;
+        }
+    }
+}
