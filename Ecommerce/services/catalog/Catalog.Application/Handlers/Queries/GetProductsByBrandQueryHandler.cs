143d9f552e8994a847e5be40baa78aea0cc93474
Ali-Abozaid <ali.abozaidazz@gmail.com>
2026-03-22 21:26:17 +0200

    implement CQRS GetAllProductsByBrandQueryHandler

---
+++ Ecommerce/services/catalog/Catalog.Application/Handlers/Queries/GetProductsByBrandQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using Catalog.Core.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.Handlers.Queries
+{
+    public class GetProductsByBrandQueryHandler : IRequestHandler<GetProductsByBrandQuery, IList<ProductResponseDto>>
+    {
+        private readonly IMapper _mapper;
+        private readonly IProductRepository _productRepository;
+        public GetProductsByBrandQueryHandler(IMapper mapper, IProductRepository productRepository)
+        {
+            _mapper = mapper;
+            _productRepository = productRepository;
+        }
+
+        public async Task<IList<ProductResponseDto>> Handle(GetProductsByBrandQuery request, CancellationToken cancellationToken)
+        {
+           var products = await _productRepository.GetAllProductsByBrand(request.ProductBrand);
+           var productsResponse = _mapper.Map<IList<ProductResponseDto>>(products);
+              return productsResponse;
+        }
+    }
+}
---
+++ Ecommerce/services/catalog/Catalog.Application/Queries/GetProductsByBrandQuery.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.Application.Responses;
+using Catalog.Core.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.Queries
+{
+    public class GetProductsByBrandQuery:IRequest<IList<ProductResponseDto>>
+    {
+        public string ProductBrand { get; set; }
+        public GetProductsByBrandQuery(string brand)
+        {
+            ProductBrand = brand;
+        }
+    }
+}
