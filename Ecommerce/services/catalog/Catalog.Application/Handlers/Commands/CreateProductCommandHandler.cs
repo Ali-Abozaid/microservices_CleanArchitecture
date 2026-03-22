37a4ce25d0d3043766947835c519fdbe911455b8
Ali-Abozaid <ali.abozaidazz@gmail.com>
2026-03-22 22:18:05 +0200

    CreateProductCommandHandler

---
+++ Ecommerce/services/catalog/Catalog.Application/Handlers/Commands/CreateProductCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Catalog.Application.Commands;
+using Catalog.Application.Responses;
+using Catalog.Core.Entities;
+using Catalog.Core.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.Handlers.Commands
+{
+    public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, ProductResponseDto>
+    {
+        private readonly IMapper _mapper;
+        private readonly IProductRepository _productRepository;
+        public CreateProductCommandHandler(IMapper mapper, IProductRepository productRepository)
+        {
+            _mapper = mapper;
+            _productRepository = productRepository;
+        }
+        public async Task<ProductResponseDto> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            var product = _mapper.Map<Product>(request);
+            var newProduct = await _productRepository.CreateProduct(product);
+            var productResponse = _mapper.Map<ProductResponseDto>(newProduct);
+            return productResponse;
+        }
+    }
+}
