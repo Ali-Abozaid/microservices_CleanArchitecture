65082039b66c253ab7e15b1da48101590bd6a370
Ali-Abozaid <ali.abozaidazz@gmail.com>
2026-03-23 15:45:37 +0200

    implement CQRS update and delete command

---
+++ Ecommerce/services/catalog/Catalog.Application/Commands/DeleteProductCommand.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.Core.Entities;
+using MediatR;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.Commands
+{
+    public class DeleteProductCommand:IRequest<bool>
+    {
+        public string Id { get; set; }
+        public DeleteProductCommand(string id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ Ecommerce/services/catalog/Catalog.Application/Handlers/Commands/DeleteProductCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Catalog.Application.Commands;
+using Catalog.Core.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.Handlers.Commands
+{
+    public class DeleteProductCommandHandler : IRequestHandler<DeleteProductCommand, bool>
+    {
+        private readonly IProductRepository _productRepository;
+        
+
+        public DeleteProductCommandHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+            
+
+        }
+        public async Task<bool> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+        {
+            // 1) Get existing product
+            var existingProduct = await _productRepository.GetProductById(request.Id);
+
+            if (existingProduct == null)
+                return false;
+            return await _productRepository.DeleteProduct(existingProduct.Id);
+
+        }
+    }
+}
